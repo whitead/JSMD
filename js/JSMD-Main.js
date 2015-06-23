c32c5054a2fcb62ffd2b74fb4064de55de6807ce
Andrew White <white.d.andrew@gmail.com>
2015-06-23 11:10:20 -0400

    Added a pause button and tweaked the style a little bit

--- js/JSMD-Main.js
+++ js/JSMD-Main.js
@@ -2,11 +2,16 @@ function main() {
     
     var root = document.getElementById( 'JSMD' );
     
-    var elements = [new Sim([10,10,10], window.innerWidth, window.innerHeight)];
-    elements[0].set_positions(square_lattice([7,7,7], [10 / 7.0,10 / 7.0,10 / 7.0],[1,1,1]));
+    var elements = [new Sim([7,7,7], window.innerWidth, window.innerHeight)];
+    elements[0].set_positions(square_lattice([7,7,7], [1,1,1],[1,1,1]));
     var scene = new Scene(elements, root);    
     scene.animate();
 
     //this will draw the 0th particle's neighbors
     var nd =  new NeighborDrawer(elements[0], scene, 0);
+
+    //bind the pause button
+    document.getElementById('pause').onclick = function() {
+	elements[0].toggle_pause();
+    };
 }
