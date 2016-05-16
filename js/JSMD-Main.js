7eeabe26c5c1bef77e6ccc1b87b61ad922a67d93
Andrew White <white.d.andrew@gmail.com>
2016-05-16 12:39:40 -0400

    Some work on multiple scenes

--- js/JSMD-Main.js
+++ js/JSMD-Main.js
@@ -3,60 +3,25 @@
 function main() {
     
 
-    var root = document.getElementById( 'JSMD' );
-
-    var elements = [new Sim([7,7,1], window.innerWidth/2.0, window.innerHeight/2.0, 2),
-		    new Sim([7,7,1], window.innerWidth/2.0, window.innerHeight/2.0, 2),
-            new Sim([7,7,1], window.innerWidth/2.0, window.innerHeight/2.0, 2),
-		   ];
-  
-    elements[0].set_positions(square_lattice([7,7,1], 1, 0.5));    
-    elements[1].set_positions(square_lattice([7,7,1], 1.0, 0.5));
-    elements[2].set_positions(square_lattice([7,7,1], 1.0, 0.5));
-
-    elements[0].T = 0.1;
-    elements[1].T = 0.4;
-    elements[2].T = 1.1;
-
-    var m = new THREE.Matrix4();
-    m.makeTranslation(window.innerWidth / 6, 0, 0);
-    elements[2].transform.premultiply(m);
-
-    m = new THREE.Matrix4();
-    m.makeTranslation(-window.innerWidth / 6, 0, 0);
-    elements[0].transform.premultiply(m);
-
-
- /*   
-    var elements = [new Sim([2.5,1,0], window.innerWidth, window.innerHeight, 1, 10),
-		   // new Sim([7,1,1], window.innerWidth, window.innerHeight, 2, 10),
-		   ];
+    var m = new MultiScene(true);
     
+    var root = document.getElementById( 'JSMD-1' );
+    var elements = [new Sim([3,3,3], window.innerWidth, window.innerHeight, 3)];  
+    elements[0].set_positions(square_lattice([3,3,3], 1, 0.5));    
+    m.add_scene(elements, root, false);    
+
+    root = document.getElementById( 'JSMD-2' );
+    elements = [new Sim([7,7,1], window.innerWidth, window.innerHeight, 2)];  
+    elements[0].set_positions(square_lattice([7,7,1], 1, 0.5));
+    m.add_scene(elements, root, true);        
+
+    root = document.getElementById( 'JSMD-3' );
+    elements = [new Sim([7,7,1], window.innerWidth, window.innerHeight, 1)];  
     elements[0].set_positions(square_lattice([1,1,1], 1, 2.49));
     elements[0].set_field_force(parabola_field, true);
     elements[0].T = null;
-    //elements[1].set_positions(square_lattice([7,1,1], 1.0, 0.5));
-   
+    m.add_scene(elements, root,  true);    
 
-    var m = new THREE.Matrix4();
-    //m.makeTranslation(window.innerWidth / 16, 0, 0);
-    elements[0].transform.premultiply(m);
 
-    //flip it for fun?
-    //m.makeTranslation(-window.innerWidth / 6, 0, 0);    
-    //elements[1].transform.premulthiply(m);
-
-    watch_com(elements[0]);
-
- */   
-    var scene = new Scene(elements, root, true);    
-    scene.animate();
-    
-    //bind the pause button
-    document.getElementById('pause').onclick = function() {
-	   elements[0].toggle_pause();
-       elements[1].toggle_pause();
-       elements[2].toggle_pause();
-
-    };
+    m.animate();
 }
--- js/JSMD-Scene.js
+++ js/JSMD-Scene.js
@@ -1,6 +1,6 @@
 //JSMD-Scene.js
 
-function Scene(elements, root, ortho, controls, light_background, fov) {
+function Scene(elements, root, ortho, light_background, stats, controls, fov, renderer) {
 
     this.elements = elements;
     this.root = root;
@@ -8,7 +8,6 @@ function Scene(elements, root, ortho, controls, light_background, fov) {
     ortho = ortho || false;
     controls = controls || true;
     fov = fov || 75;
-    light_background = light_background || true;
      
      if(ortho) {
 	 var factor = 0.5 * Math.tan(fov / 360 * Math.PI);
@@ -26,37 +25,48 @@ function Scene(elements, root, ortho, controls, light_background, fov) {
     }
 
     this.camera.position.z = Math.min(this.root.clientWidth, this.root.clientHeight);	     
-     this.scene = new THREE.Scene();
-     var scene = this.scene;
+    this.scene = new THREE.Scene();
+
      
-     
-    this.renderer = new THREE.WebGLRenderer( { clearAlpha: 1, antialias: true } );
+
+
+    if(!renderer) {
+	this.renderer = new THREE.WebGLRenderer( { clearAlpha: 1, antialias: true } );
     
-    this.renderer.setPixelRatio( window.devicePixelRatio );
-    this.renderer.setSize( this.root.clientWidth, this.root.clientHeight );
-    this.renderer.setClearColor( 0x000000, 1 );    
-    if(light_background)
-	this.renderer.setClearColor( 0xFFFFFF, 1 );
+	this.renderer.setPixelRatio( window.devicePixelRatio );
+	this.renderer.setSize( this.root.clientWidth, this.root.clientHeight );
+	this.renderer.setClearColor( 0x000000, 1 );
+	
+	root.appendChild( this.renderer.domElement );
+	window.addEventListener( 'resize', this.onWindowResize.bind(this), false );     
+	
+	if(light_background)
+	    this.renderer.setClearColor( 0xFFFFFF, 1 );
+    } else {
+	this.renderer = renderer;
+    }
     
-     root.appendChild( this.renderer.domElement );
-     window.addEventListener( 'resize', this.onWindowResize.bind(this), false );     
-          
+    var scene = this.scene;    
      elements.forEach(function(e) {
 	 e.init_render(scene);
      });
+
+    if(stats) {
+	//set-up the performance stats
+	this.do_stats = true;
+	this.stats = new Stats();
+	
+	// align bottom-left
+	this.stats.domElement.style.position = 'absolute';
+	this.stats.domElement.style.left = '0px';
+	this.stats.domElement.style.bottom = '0px';
+	
+	root.appendChild( this.stats.domElement );
+    } else {
+	this.do_stats = false;
+    }
      
-     //set-up the performance stats
-     this.stats = new Stats();
-     
-     // align bottom-left
-     this.stats.domElement.style.position = 'absolute';
-     this.stats.domElement.style.left = '0px';
-     this.stats.domElement.style.bottom = '0px';
-     
-     root.appendChild( this.stats.domElement );
-     
-     
-     
+
  }
 
 Scene.prototype.onWindowResize = function() {
@@ -74,8 +84,7 @@ Scene.prototype.animate = function() {
     
     requestAnimationFrame(this.animate.bind(this));
     this.render();
-    this.stats.update();
-};
+}
 
 
 Scene.prototype.render = function() {
@@ -83,8 +92,53 @@ Scene.prototype.render = function() {
     this.elements.forEach(function(e) {
 	e.animate();
     });
+
+    if(this.do_stats)
+	this.stats.update();
+    
     
     this.renderer.render( this.scene, this.camera );
     
 }
 
+
+
+function MultiScene(light_background) {
+
+    this.renderer = new THREE.WebGLRenderer( { clearAlpha: 1, antialias: true } );
+    
+    this.renderer.setPixelRatio( window.devicePixelRatio );
+    //this.renderer.setSize( this.root.clientWidth, this.root.clientHeight );
+    this.renderer.setClearColor( 0x000000, 1 );    
+    if(light_background)
+	this.renderer.setClearColor( 0xFFFFFF, 1 );
+
+    this.scenes = [];
+}
+
+MultiScene.prototype.add_scene = function(elements, root,  ortho, stats, controls, fov) {
+    this.scenes.push(new Scene(elements, root, ortho, false, stats, controls, fov, this.renderer)); 
+}
+
+
+MultiScene.prototype.animate = function() {
+
+    this.render();    
+    requestAnimationFrame(this.animate.bind(this));
+}
+
+MultiScene.prototype.render = function() {
+    
+    this.scenes.forEach(function(s) {
+	s.render();
+    });
+}
+
+
+MultiScene.prototype.updateSize = function() {
+    var width = canvas.clientWidth;
+    var height = canvas.clientHeight;
+    if ( canvas.width !== width || canvas.height != height ) {
+	renderer.setSize( width, height, false );
+    }
+}
