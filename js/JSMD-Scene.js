60c6dc88f88627f7a70d0c6456e36d3a59bf1235
ZHANGYIXUAN <zhangyixuan@dhcp-10-5-22-242.wireless.rochester.edu>
2016-08-03 15:58:10 -0400

    Seperate each simulation in a single scene and rearrange it in CSS

--- js/JSMD-Scene.js
+++ js/JSMD-Scene.js
@@ -1,90 +1,47 @@
 //JSMD-Scene.js
 
-function Scene(elements, root, ortho, controls, light_background, fov) {
+function Scene(element, root, T, renderer,ortho, controls, fov) {
 
-    this.elements = elements;
+    var content = document.getElementById( "content" );
+    var template = document.getElementById( "template" ).text;
+
+    this.element = element;
     this.root = root;
-     
+    this.renderer = renderer;
+
+    //Make a list element
+    var divelement = document.createElement( "div" );
+    divelement.className = "list-element";
+    divelement.innerHTML = template.replace( '$', T );
+
+    this.divelement = divelement;
+    content.appendChild( this.divelement );
+
+    //look up the area that we want to render
+    this.divelementscene=this.divelement.querySelector( ".scene" );
+
     ortho = ortho || false;
     controls = controls || true;
     fov = fov || 75;
-    light_background = light_background || true;
-     
-     if(ortho) {
-	 var factor = 0.5 * Math.tan(fov / 360 * Math.PI);
-	 this.camera = new THREE.OrthographicCamera( this.root.clientWidth * -factor, this.root.clientWidth * factor, this.root.clientHeight * factor, this.root.clientHeight * -factor, 1, 10000 );
-	 this.camera.zoom = 1;
-	 
-     } else{
-	 this.camera = new THREE.PerspectiveCamera(75, this.root.clientWidth / this.root.clientHeight, 1, 10000);	 
-     }
+
+	 this.camera = new THREE.PerspectiveCamera(75, 1, 1, 10000);
+
 
     if(controls) {
-	 this.controls = new THREE.OrbitControls( this.camera, root );
+	 this.controls = new THREE.OrbitControls( this.camera, this.divelementscene );
 	 this.controls.damping = 0.2;
-	 this.controls.addEventListener( 'change', this.render.bind(this) );
+   this.controls.enablePan = false;
+	 //this.controls.addEventListener( 'change', this.render.bind(this) );
     }
 
-    this.camera.position.z = Math.min(this.root.clientWidth, this.root.clientHeight);	     
-     this.scene = new THREE.Scene();
-     var scene = this.scene;
-     
-     
-    this.renderer = new THREE.WebGLRenderer( { clearAlpha: 1, antialias: true } );
-    
-    this.renderer.setPixelRatio( window.devicePixelRatio );
-    this.renderer.setSize( this.root.clientWidth, this.root.clientHeight );
-    this.renderer.setClearColor( 0x000000, 1 );    
-    if(light_background)
-	this.renderer.setClearColor( 0xFFFFFF, 1 );
-    
-     root.appendChild( this.renderer.domElement );
-     window.addEventListener( 'resize', this.onWindowResize.bind(this), false );     
-          
-     elements.forEach(function(e) {
-	 e.init_render(scene);
-     });
-     
-     //set-up the performance stats
-     this.stats = new Stats();
-     
-     // align bottom-left
-     this.stats.domElement.style.position = 'absolute';
-     this.stats.domElement.style.left = '0px';
-     this.stats.domElement.style.bottom = '0px';
-     
-     root.appendChild( this.stats.domElement );
-     
-     
-     
- }
+    this.camera.position.z = Math.min(this.root.clientWidth, this.root.clientHeight);
+    this.scene = new THREE.Scene();
+    var scene = this.scene;
 
-Scene.prototype.onWindowResize = function() {
-    
-    this.camera.aspect = this.root.clientWidth / this.root.clientHeight;
-    this.camera.updateProjectionMatrix();
-    
-    this.renderer.setSize( this.root.clientWidth, this.root.clientHeight );
-    
-    this.render();
-    
+    //this.root.appendChild( renderer.domElement );
+    //window.addEventListener( 'resize', this.onWindowResize.bind(this), false );
+
+
+    element.init_render(scene);
+
 }
-
-Scene.prototype.animate = function() {
-    
-    requestAnimationFrame(this.animate.bind(this));
-    this.render();
-    this.stats.update();
-};
-
-
-Scene.prototype.render = function() {
-    
-    this.elements.forEach(function(e) {
-	e.animate();
-    });
-    
-    this.renderer.render( this.scene, this.camera );
-    
-}
-
--- js/JSMD-Sim-Decorators.js
+++ js/JSMD-Sim-Decorators.js
@@ -1,18 +1,18 @@
 //JSMD-Sim-Decorators.js
 
 function NeighborDrawer(sim, scene, index) {
-    
+
     this.index = index;
 
     var m = new THREE.MeshBasicMaterial( { wireframe: true, wireframeLineWidth: 0, color:0x44EE22} );
     this.neighbor_material = new THREE.MeshBasicMaterial( { wireframe: true, wireframeLineWidth: 0, color:0xFF00EF} );
 
-    
+
     this.sphere_geom = new THREE.SphereGeometry(sim.particle_radius * 0.3, 12, 12);
     this.origin_sphere = new THREE.Mesh(this.sphere_geom, m);
     this.origin_index = sim.add_mesh(this.origin_sphere);
     this.neighbor_meshes = [];
-    
+
     //create closure and register
     var that = this;
     sim.add_update_listener(this);
@@ -24,19 +24,19 @@ NeighborDrawer.prototype.set_index = function(index) {
 
 NeighborDrawer.prototype.update = function(sim) {
     var i,j;
-    
+
     //update origin
     this.origin_sphere.position.x = sim.positions[this.index][0];
     this.origin_sphere.position.y = sim.positions[this.index][1];
     this.origin_sphere.position.z = sim.positions[this.index][2];
-    
-    
+
+
     //update neighbors
     for(i = 0; i < this.neighbor_meshes.length; i++) {
 	//have we reached the end?
 	if(sim.neighbor_array[this.index][i] === -1) {
 	    //if so, remove the extra meshes we have
-	    for(j = 0; j < (this.neighbor_meshes.length - i); j++) {	
+	    for(j = 0; j < (this.neighbor_meshes.length - i); j++) {
 		sim.remove_mesh(this.neighbor_meshes.pop());
 	    }
 	    break;
@@ -45,22 +45,22 @@ NeighborDrawer.prototype.update = function(sim) {
 	this.neighbor_meshes[i].position.y = sim.positions[sim.neighbor_array[this.index][i]][1];
 	this.neighbor_meshes[i].position.z = sim.positions[sim.neighbor_array[this.index][i]][2];
     }
-    
+
     //did we not have enough meshes for neighbors??
     while(sim.neighbor_array[this.index][i] !== -1) {
 	//create a new mesh
 	var m = new THREE.Mesh(this.sphere_geom, this.neighbor_material);
-	
+
 	//update the positions
 	m.position.x = sim.positions[sim.neighbor_array[this.index][i]][0];
 	m.position.y = sim.positions[sim.neighbor_array[this.index][i]][1];
 	m.position.z = sim.positions[sim.neighbor_array[this.index][i]][2];
-	
-	
+
+
 	//add it to both the simulation and us
 	this.neighbor_meshes.push(m)
 	sim.add_mesh(m);
-	
+
 	i += 1;
     }
 }
@@ -71,20 +71,20 @@ function Colvar(sim, fxn, name, plot_id) {
     this.fxn = fxn;
     if(name && plot_id){
 	this.do_plot = true
-	this.chart = create_plots(plot_id);	
+	this.chart = create_plots(plot_id);
     }
-    
+
     //create closure and register
     var that = this;
     sim.add_update_listener(this);
-    
+
 }
 
 Colvar.prototype.update = function(sim) {
-    var value = this.fxn(sim.positions, sim.velocities, sim.dimension);    
-    update_plot(this.chart, value);    
+    var value = this.fxn(sim.positions, sim.velocities, sim.dimension);
+    update_plot(this.chart, value);
 }
-    
+
 function watch_com(sim) {
     var c = new Colvar(sim, function(p, v, d) {
 	var value = 0;
